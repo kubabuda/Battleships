94c55795e1fcc63562e38f835f4896bdfb547834
Kuba Buda <kubabuda@gazeta.pl>
2019-09-09 22:34:26 +0200

    Install NSubstitute mock lib

---
+++ Battleships.IntegrationTests/Services/BattleshipGame.IntegrationTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Battleshp.Services.IntegrationTests
+{
+    public class BattleshipGameTests
+    {
+        private IConsole console;
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        public void Play_ShouldShowUntouchedGrid_OnFirstRound(){
+            // arrange
+            
+        }
+    }
+}
