261a312a8c117437c01f7dca9947bc3e032570c2
Kuba Buda <kubabuda@gazeta.pl>
2019-09-12 22:18:25 +0200

    Refactor: move integration tests to separate file

--- Battleships.IntegrationTests/Services/BattleshipGame.IntegrationTests.cs
+++ Battleships.IntegrationTests/Services/BattleshipGame.IntegrationTests.cs
@@ -6,7 +6,6 @@ using Battleships.Services;
 using NSubstitute;
 using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Battleship.Services.IntegrationTests
 {
@@ -186,49 +185,6 @@ namespace Battleship.Services.IntegrationTests
                 _container.Resolve<IConsole>(),
                 _container.Resolve<IBattleshipStateBuilder>(),
                 prevState);
-        }
-    }
-
-    public class BattleshipStateBuilderIntegrationTests
-    {
-        private Configuration _config;
-        private IBattleshipStateBuilder _stateBuilder;
-
-        [SetUp]
-        public void SetUp()
-        {
-            // setup mocked components
-            _config = new Configuration();
-            _config.Ships = new List<int>();
-            var console = Substitute.For<IConsole>();
-            // use DI setup for rest
-            var builder = Bootstrapper.GetContainerBuilder();
-            // register mocks
-            builder.RegisterInstance<IConsole>(console);
-            builder.RegisterInstance<IConfiguration>(_config);
-            
-            var container = builder.Build();
-            // get tested class instance
-            _stateBuilder = container.Resolve<IBattleshipStateBuilder>();
-        }
-
-        [TestCase(new [] { 2, 3, 4 })]
-        [TestCase(new [] { 4, 3, 3 })]
-        public void Build_ShouldPlaceAllTheShips_FromConfiguration(IEnumerable<int> ships)
-        {
-            // arrange
-            _config.Ships = new List<int>(ships);
-            int expectedShips = ships.Sum();
-            int expectedEmptyCells = (_config.GridSize * _config.GridSize) - expectedShips;
-
-            // act
-            var result = _stateBuilder.Build();
-
-            // assert
-            int shipsOnBoard = result.Grid.Sum(line => line.Where(c => c == BattleshipGridCell.ShipUntouched).Count());
-            Assert.AreEqual(expectedShips, shipsOnBoard);
-            int emptyCells = result.Grid.Sum(line => line.Where(c => c == BattleshipGridCell.Empty).Count());
-            Assert.AreEqual(expectedEmptyCells, emptyCells);
         }
     }
 }
---
+++ Battleships.IntegrationTests/Services/BattleshipStateBuilderIntegrationTests.cs
@@ -0,0 +1,54 @@
+using Autofac;
+using Battleships;
+using Battleships.Interfaces;
+using Battleships.Models;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Battleship.Services.IntegrationTests
+{
+    public class BattleshipStateBuilderIntegrationTests
+    {
+        private Configuration _config;
+        private IBattleshipStateBuilder _stateBuilder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // setup mocked components
+            _config = new Configuration();
+            _config.Ships = new List<int>();
+            var console = Substitute.For<IConsole>();
+            // use DI setup for rest
+            var builder = Bootstrapper.GetContainerBuilder();
+            // register mocks
+            builder.RegisterInstance<IConsole>(console);
+            builder.RegisterInstance<IConfiguration>(_config);
+            
+            var container = builder.Build();
+            // get tested class instance
+            _stateBuilder = container.Resolve<IBattleshipStateBuilder>();
+        }
+
+        [TestCase(new [] { 2, 3, 4 })]
+        [TestCase(new [] { 4, 3, 3 })]
+        public void Build_ShouldPlaceAllTheShips_FromConfiguration(IEnumerable<int> ships)
+        {
+            // arrange
+            _config.Ships = new List<int>(ships);
+            int expectedShips = ships.Sum();
+            int expectedEmptyCells = (_config.GridSize * _config.GridSize) - expectedShips;
+
+            // act
+            var result = _stateBuilder.Build();
+
+            // assert
+            int shipsOnBoard = result.Grid.Sum(line => line.Where(c => c == BattleshipGridCell.ShipUntouched).Count());
+            Assert.AreEqual(expectedShips, shipsOnBoard);
+            int emptyCells = result.Grid.Sum(line => line.Where(c => c == BattleshipGridCell.Empty).Count());
+            Assert.AreEqual(expectedEmptyCells, emptyCells);
+        }
+    }
+}
