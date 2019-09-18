8dd8f1a399f1d9eaf8ff64b60df5d6a8bb9d6c94
Kuba Buda <kubabuda@gazeta.pl>
2019-09-18 16:34:32 +0200

    Remaining integration tests of user input parsing are now in specflow too

---
+++ Battleships.IntegrationTests/Services/ReadUserGuess.feature
@@ -0,0 +1,36 @@
+﻿Feature: ReadUserGuess
+	In order to read user input
+	As a someone playing Battleships
+	I want to get user inpud translated to grid coordinates, or exception
+
+
+Scenario Outline: Reading valid guess
+	Given New reader instance
+	When I read '<Guess>'
+	Then '<Line>' '<Column>' are read
+
+	Examples: 
+		| Guess | Line | Column |
+	    | B2  | 1 | 1 |
+	    | A1  | 0 | 0 |
+	    | A10 | 0 | 9 |
+	    | J1  | 9 | 0 |
+	    | J10 | 9 | 9 |
+
+  
+Scenario Outline: Reading invalid guess
+	Given New reader instance
+	When I read '<Guess>'
+	Then InvalidInputException is thrown by reader
+
+	Examples: 
+		| Guess  |
+	    | B22    |
+		| AA1    |
+	    | ASAP10 |
+	    | K1     |
+		| FOO    |
+	    | A0     |
+		| J0     |
+	    | A      |
+		| J      |
---
+++ Battleships.IntegrationTests/Services/ReadUserGuessSteps.cs
@@ -0,0 +1,55 @@
+﻿using Autofac;
+using Battleships.Configurations;
+using Battleships.Interfaces;
+using Battleships.Models;
+using NSubstitute;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Battleships.IntegrationTests.Services
+{
+    [Binding]
+    public class ReadUserGuessSteps
+    {
+        private IReadUserGuess _serviceUnderTest;
+        private string _guess;
+
+        [Given(@"New reader instance")]
+        public void GivenNewReaderInstance()
+        {
+            // setup mocked components
+            var _config = Substitute.For<IBattleshipsConfiguration>();
+            var grid_size = 10;
+            _config.GridSize.Returns(grid_size);
+            // use DI setup for rest
+            var builder = Bootstrapper.GetContainerBuilder(_config);
+            // register mocks
+            builder.RegisterInstance<IBattleshipsConfiguration>(_config);
+
+            var container = builder.Build();
+            // get tested class instance
+            _serviceUnderTest = container.Resolve<IReadUserGuess>();
+        }
+
+        [When(@"I read '(.*)'")]
+        public void WhenIRead(string guess)
+        {
+            _guess = guess;
+        }
+
+        [Then(@"'(.*)' '(.*)' are read")]
+        public void ThenAreRead(int line, int column)
+        {
+            var result = _serviceUnderTest.GetCordinates(_guess);
+
+            Assert.AreEqual(line, result.Line);
+            Assert.AreEqual(column, result.Column);
+        }
+
+        [Then(@"InvalidInputException is thrown by reader")]
+        public void ThenInvalidInputExceptionIsThrownByReader()
+        {
+            Assert.Throws<InvalidInputException>(() => _serviceUnderTest.GetCordinates(_guess));
+        }
+    }
+}
